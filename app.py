5e916e383432834451e1a9eb234a87930375a283
Muhammad Azeem <61928905+Azeemaj101@users.noreply.github.com>
2022-03-05 23:59:59 +0500

    initial commit

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template, request, redirect
-from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime
+from flask import Flask
+# from flask import Flask, render_template, request, redirect
+# from flask_sqlalchemy import SQLAlchemy
+# from datetime import datetime
 app = Flask(__name__)
 
 # app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:////Projects/Web Projects/Web-Projects/Flask/todo/todo.db"
