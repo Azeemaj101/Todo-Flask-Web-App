43b27a7d361695b9f7a678910e79d5a02645fded
Muhammad Azeem <61928905+Azeemaj101@users.noreply.github.com>
2022-03-06 13:03:05 +0500

    initial commit

--- app.py
+++ app.py
@@ -4,7 +4,8 @@ from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 app = Flask(__name__)
 
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:////Projects/Web Projects/Web-Projects/Flask/todo/todo.db"
+# app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:////Projects/Web Projects/Web-Projects/Flask/todo/todo.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:////todo.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
 
